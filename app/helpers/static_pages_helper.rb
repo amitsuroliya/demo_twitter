5f4d792a01ab82183352e42ec9a544d94a5fd147
amitsuroliya <amitkumarsuroliya@gmail.com>
2012-09-04 11:44:41 +0530

    Finish layout and routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,3 +1,2 @@
 module StaticPagesHelper
-	
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,15 +7,15 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('h1', text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
+    it { should have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_selector('title', text: full_title('Help')) }
   end
 
