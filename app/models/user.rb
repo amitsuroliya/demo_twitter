2d1f77ba0f23de489716a3bc3bd7ba18d0de2cb3
amitsuroliya <amitkumarsuroliya@gmail.com>
2012-09-05 19:04:11 +0530

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { self.email.downcase! }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,20 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
   
   describe "when name is not present" do
     before { @user.name = " " }
