efb6264de9c6d7291fc02aa467c90cea98648cc0
amitsuroliya <amitkumarsuroliya@gmail.com>
2012-09-07 19:23:40 +0530

    Finish Chepter 11 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -114,6 +114,28 @@ describe "User pages" do
         end
       end
     end
+
+    describe "user stats display correctly" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+        user.follow!(other_user)
+        other_user.follow!(user)
+        visit user_path(user)
+      end
+
+      it { should have_selector('strong#following', text:'1') }
+      it { should have_selector('strong#followers', text:'1') }
+
+      describe "after unfollowing other user" do
+        before { user.unfollow!(other_user); visit user_path(user) }
+        it { should have_selector('strong#following', text: '0') }
+      end
+
+      describe "after unfollowed by other user" do
+        before { other_user.unfollow!(user); visit user_path(user) }
+        it { should have_selector('strong#followers', text: '0') }
+      end
+    end
   end
 
   describe "signup" do
